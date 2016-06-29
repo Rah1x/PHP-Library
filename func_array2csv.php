516af573120e76a4fd1c9ef5d7ba620daf109095
Rah1x <raheel_angle@hotmail.com>
2016-06-29 19:08:05 +0500

    C4

---
+++ func_array2csv.php
@@ -0,0 +1,20 @@
+<?php
+/** another way of getting array to csv **/
+function array2csv(array &$array, $output_key=true)
+{
+   if (count($array) == 0) {
+     return null;
+   }
+   ob_start();
+   $df = fopen("php://output", 'w');
+
+   if($output_key)
+   fputcsv($df, array_keys(reset($array)));
+
+   foreach ($array as $row) {
+      fputcsv($df, $row);
+   }
+   fclose($df);
+   return ob_get_clean();
+}
+?>
