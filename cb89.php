424dae8999da846db99e87d1b95345f69dfc2424
Rah1x <raheel_angle@hotmail.com>
2016-06-29 19:00:08 +0500

    C4

--- cb89.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * function cb89
- * Set top key of a multi-dimensional array from a kay=>value in its 2nd level
- * USAGE Example: $contact_res_t = cb89($contact_res, 'id');
- */
-function cb89($a, $set_key){$ret=array(); foreach($a as $v){$ret[$v[$set_key]]=$v;} return $ret;}
-?>
